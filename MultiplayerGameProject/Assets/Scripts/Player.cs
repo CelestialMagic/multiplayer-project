948908e6cb2ab703ba9a6b1ccec8b77ab25c4b75
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-01-10 20:56:10 -0800

    Commented Code

--- MultiplayerGameProject/Assets/Scripts/Player.cs
+++ MultiplayerGameProject/Assets/Scripts/Player.cs
@@ -30,15 +30,18 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //Locks cursor for rotating with mouse
         //Cursor.lockState = CursorLockMode.Locked;
 
     }
 
+//Enables Input Actions
      void OnEnable(){
         forwardMovement.Enable();
         sideMovement.Enable();
     }
 
+//Disables Input Actions
      void OnDisable(){
         forwardMovement.Disable();
         sideMovement.Disable();
@@ -47,11 +50,14 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Gets value of key presses for side and forward movement
         float sideInput = sideMovement.ReadValue<float>();
         float forwardInput = forwardMovement.ReadValue<float>();
 
+//Converts this to a vector based on the movement
         Vector3 directionToMove = new Vector3(sideInput, 0, forwardInput);
 
+//Multiplies movement vector by deltaTime and the speed to move by
         movementForce = new Vector3(sideInput * moveSpeed * Time.deltaTime, 0, forwardInput * moveSpeed * Time.deltaTime);
         transform.Translate(movementForce, Space.World);
 
@@ -59,6 +65,7 @@ public class Player : MonoBehaviour
 
     }
 
+//Rotates the player with the mouse
     void RotateWithMouse(){
         y = Input.GetAxis("Mouse X");
         x = Input.GetAxis("Mouse Y");
