63915ead4aff9d75839bfcaee1bdb47e1d9299ef
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-01-10 20:28:00 -0800

    Added Crab 3D Model In

--- MultiplayerGameProject/Assets/Scripts/Player.cs
+++ MultiplayerGameProject/Assets/Scripts/Player.cs
@@ -30,7 +30,7 @@ public class Player : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.lockState = CursorLockMode.Locked;
 
     }
 
