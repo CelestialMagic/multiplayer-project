d5cc5a9bc6bc88cdfeabf45186568d52340fd1ff
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-01-20 09:16:31 -0800

    Added Controller Support for Jumping
    -Toggled values for player jumping

--- MultiplayerGameProject/Assets/Scripts/PlayerMovementJ.cs
+++ MultiplayerGameProject/Assets/Scripts/PlayerMovementJ.cs
@@ -13,17 +13,17 @@ Player Fields
     private float moveSpeed, rotationSpeed, groundDrag, jumpForce;//floats representing speed to move by
 
     [SerializeField]
-    private Transform orientation;
+    private Transform orientation;//Orientation of player 
 
-    Vector3 moveDirection; 
+    Vector3 moveDirection;//Direction to move by
 
-    private float sideInput, forwardInput; 
+    private float sideInput, forwardInput;//Not currently used 
 
     [SerializeField]
     private InputAction forwardMovement, sideMovement, meleeMovement, jumpMovement;//InputActions to be mapped to player actions
 
     [SerializeField]
-    private CinemachineVirtualCamera followCam;
+    private CinemachineVirtualCamera followCam;//The camera assigned to the player
 
      private float rotation;//Value of Rotation
 
@@ -32,21 +32,21 @@ Player Fields
      private float viewSpeed, viewRange;//Speed and range of view 
 
      [SerializeField]
-     private GameObject currentShell; 
+     private GameObject currentShell;//The currently equipped shell 
 
      [SerializeField]
-     private List<Shell> playerShells; 
+     private List<Shell> playerShells;//The list of available shells to swap out
 
     [SerializeField]
     private GameObject prefab, modelView;//The player's prefab (to be used later for Photon)
 
     [SerializeField]
-    private Color color;//The player color 
+    private Color color;//The player color (to be used by Photon eventually)
 
     [SerializeField]
-    private Rigidbody rb;
+    private Rigidbody rb;//The player's rigidbody
 
-    private bool isGrounded = false; 
+    private bool isGrounded = false;//Checks if player can jump again 
 
 
 
@@ -112,15 +112,19 @@ private void MovePlayer(){
     rb.AddForce(moveDirection.normalized * moveSpeed, ForceMode.Force);
 }
 
+//Gets the side input of the player 
 public float GetSideInput(){
     return sideMovement.ReadValue<float>();
 
 }
+
+//Gets the forward movement of the player
 public float GetForwardInput(){
     return forwardMovement.ReadValue<float>();
 
 }
 
+//Controls the drag of the player moving 
 private void SpeedControl(){
     Vector3 flatVelocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
 
@@ -130,7 +134,7 @@ private void SpeedControl(){
     }
 }
 
-
+//Current player rotation scheme 
 private void SimpleRotation(){
     //Rotation with Mouse Code
     rotation += -Input.GetAxis("Mouse Y") * viewSpeed;
@@ -139,6 +143,7 @@ private void SimpleRotation(){
     transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * viewSpeed, 0);
 }
 
+//A potential mode to rotate around the model
 private void ModelRotation(){
     //Rotation with Mouse Code
     rotation += -Input.GetAxis("Mouse Y") * viewSpeed;
@@ -178,6 +183,7 @@ private void OnTriggerEnter(Collider collision){
 
 }
 
+//Used to check if player is on the ground
 private void OnCollisionStay(Collision collision){
     if(collision.gameObject.tag == "Ground"){
         
@@ -186,6 +192,7 @@ private void OnCollisionStay(Collision collision){
     }
 }
 
+//Used to check if player leaves the ground 
 private void OnCollisionExit(Collision collision){
     isGrounded = false; 
     Debug.Log(isGrounded);
