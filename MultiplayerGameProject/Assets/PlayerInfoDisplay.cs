8acfc380df4bb7da520e08e61f99ab17bb7e7b3f
Celestial Flair <116318917+CelestialMagic@users.noreply.github.com>
2025-02-11 21:01:36 -0800

    Added Clickable UI Icon

--- MultiplayerGameProject/Assets/PlayerInfoDisplay.cs
+++ MultiplayerGameProject/Assets/PlayerInfoDisplay.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class PlayerInfoDisplay : MonoBehaviour
 {
@@ -11,11 +12,38 @@ public class PlayerInfoDisplay : MonoBehaviour
 
     [SerializeField]
     private Image shellIcon; 
+
+    [SerializeField]
+    private GameObject descriptionMenu;
+
+    private bool isMenuActive = false;
     
     public void SetCurrentShellDisplay(string shellName, string shellInfo, Sprite shellSprite){
         shellText.text = $"{shellName}";
         infoText.text = $"{shellInfo}";
         shellIcon.sprite = shellSprite; 
+
+    }
+
+
+    public void OnPointerEnter(PointerEventData eventData){
+        Debug.Log("Pointer on Icon");
+        descriptionMenu.SetActive(true);
+
+    }
+    public void OnPointerExit(PointerEventData eventData){
+        descriptionMenu.SetActive(false);
+        Debug.Log("Pointer off Icon");
+
+    }
+
+    public void OnMouseOver(){
+        Debug.Log("Pointer on Icon");
+    }
+
+    public void ViewStats(){
+        isMenuActive = !isMenuActive;
+        descriptionMenu.SetActive(isMenuActive);
 
     }
 
